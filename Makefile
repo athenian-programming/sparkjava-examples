698d0bb0b8e51cdae671b162ba58bb5a228dd050
pambrose <pambrose@mac.com>
2019-12-22 10:01:02 -0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,12 @@
+default: compile
+
+clean:
+	./gradlew clean
+
+compile:
+	./gradlew build -xtest
+
+build: compile
+
+versioncheck:
+	./gradlew dependencyUpdates
---
+++ src/main/java/org/athenian/HttpServer.java
@@ -0,0 +1,54 @@
+package org.athenian;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static spark.Spark.*;
+
+public class HttpServer {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(HttpServer.class);
+
+        port(8080);
+
+        staticFiles.location("/public");
+
+        before((req, res) -> {
+            logger.info("request: {} {}", req.ip(), req.pathInfo());
+        });
+
+        get("/",
+                (req, res) -> {
+                    res.type("text/plain");
+                    return "You have reached index.html";
+                });
+
+        get("/hello-plain",
+                (req, res) -> {
+                    res.type("text/plain");
+                    return "Hello World";
+                });
+
+        get("/hello-html",
+                (req, res) -> {
+                    res.type("text/html");
+                    return "<html>" +
+                            "  <head>" +
+                            "    <title>Hello</title>" +
+                            "  </head>" +
+                            "  <body>" +
+                            "    <h1>Hello World</h1>" +
+                            "  </body>" +
+                            "</html>";
+                });
+
+        get("/params",
+                (req, res) -> {
+                    res.type("text/plain");
+                    return "p1 = " + req.queryParams("p1") + "\n" +
+                            "p2 = " + req.queryParams("p2");
+                });
+    }
+}
